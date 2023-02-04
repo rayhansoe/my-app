beea3edb014ef12a1406da6e3625f38b51f5a663
rayhan <rayhan.rhssoe2@gmail.com>
2023-02-04 15:15:07 +0700

    debounce and input.

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,14 +1,31 @@
-import { Show } from "solid-js";
-import { A } from "solid-start";
+import { debounce } from "@solid-primitives/scheduled";
+import { createSignal, Show } from "solid-js";
+import { A, useLocation } from "solid-start";
 import { trpc } from "../utils/trpc";
 
 export default function Page(props: { name: string }) {
 	const hello = trpc.example.hello.useQuery(() => ({ name: props.name }));
 	let aRef: HTMLAnchorElement | ((el: HTMLAnchorElement) => void) | any;
+
+	const [value, setValue] = createSignal("");
+	const location = useLocation();
+
+	const navigate = () => aRef.click();
+
+	const debouncedNavigate = debounce(navigate, 50);
+
+	const update = (newValue: string) => {
+		if (newValue.length && newValue !== value()) {
+			setValue(`${location.pathname}?q=${newValue}`);
+			debouncedNavigate();
+		}
+	};
+
+	const debouncedUpdate = debounce(update, 500);
+
 	return (
 		<>
-			<button
-				// <A
+			{/* <button
 				onClick={() => {
 					aRef?.click();
 				}}
@@ -21,8 +38,20 @@ export default function Page(props: { name: string }) {
 				<div class='text-lg'>
 					Learn more about Create JD App, the libraries it uses, and how to deploy it
 				</div>
-				{/* </A> */}
-			</button>
+			</button> */}
+
+			<input
+				type='text'
+				onInput={(e) => debouncedUpdate(e.currentTarget.value)}
+				onKeyUp={(e) => {
+					e.preventDefault();
+					if (e.key === "Enter") {
+						debouncedUpdate.clear();
+						update(e.currentTarget.value);
+					}
+				}}
+				value={value()}
+			/>
 
 			<A ref={aRef} href='/post'>
 				{props.name}
